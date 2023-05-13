839f9ad90da76f0a1f0b494d8995cb02f0aea188
happys <happys2333@outlook.com>
2023-05-14 00:37:34 +0800

    Create task_factor.sh

---
+++ task_factor.sh
@@ -0,0 +1,48 @@
+#!/bin/bash
+
+#SBATCH -o job.%j.out
+#SBATCH --partition=gpulab02
+#SBATCH -J pytorch_transConv
+#SBATCH -N 1
+#SBATCH --ntasks-per-node=6
+#SBATCH --gres=gpu:1
+#SBATCH --qos=gpulab02
+
+# Define variables
+env_name="trans"
+seq_lens=(336)
+label_lens=(24 48 96 192)
+pred_lens=(336)
+features=("M" "S")
+python_scripts=("pred_ours.py")
+data_sets=("ETTh1" "ETTh2" "ETTm1" "ETTm2" "WTH" "ECL")
+enc_layers=(3)
+n_heads=(8 9 10)
+factor=(3 4 5 6 7)
+d_models=(64 128 256 512 1024)
+
+source activate $env_name
+
+for task in "${factor[@]}"
+do
+  for feature in "${features[@]}"
+do
+    for data_set in "${data_sets[@]}"
+    do
+        for pred_len in "${pred_lens[@]}"
+        do
+            # Loop through Python scripts
+            for python_script in "${python_scripts[@]}"
+            do
+                # Print prompt message with script name and parameters, and write it to output file
+                prompt_message=("Processing script '$python_script' with parameters: factor=$task, pred_len=$pred_len, features=$feature, dataset=$data_set")
+
+                echo "${prompt_message[@]}"
+
+                # Run current Python script with specified parameters and append output to file
+                python $python_script --factor $task --pred_len $pred_len --features $feature --dataset $data_set
+            done
+        done
+    done
+done
+done
